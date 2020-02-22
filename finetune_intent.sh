2308fe94c32bbd0655d1ac38131b61db0780726b
@charliesmail20221030 <charliesmail20221030@gmail.com>
2020-02-22 11:07:06 +0800

    feat: finetune sh script, data, and others

---
+++ finetune_intent.sh
@@ -0,0 +1,19 @@
+pip install -r requirements.txt;
+python -m run_classifier \
+--data_dir=data_dir \
+--output_dir=output_dir \
+--init_checkpoint=data_dir/albert_base/model.ckpt-best \
+--albert_config_file=data_dir/albert_base/albert_config.json \
+--do_train=true \
+--do_lower_case=true \
+--max_seq_length=128 \
+--optimizer=adamw \
+--task_name=intent \
+--warmup_step=1000 \
+--learning_rate=3e-5 \
+--train_step=10000 \
+--save_checkpoints_steps=100 \
+--train_batch_size=128
+#   --do_eval=true \
+#   --do_predict \
+#   --spm_model_file=... \
